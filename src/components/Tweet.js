a88fe3fb08c0b071c54aba81e71188ad7347a2f9
Avijit Nanda <avijitnanda14@gmail.com>
2020-06-17 02:29:06 +0530

    added react-icons library

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -8,9 +8,12 @@ const Tweet = ({ authedUser, tweet }) => {
 
 const mapStateToProps = ({ authedUser, users, tweets }, { id }) => {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 };
 
